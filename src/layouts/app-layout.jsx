9a6444db33f043c981330bc7d154ba374acba0ba
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-11 12:32:57 +0530

    Setup project with Supabase, React Router, and Tailwind; added basic routes and initial structure

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main>
+        {/* Header */}
+        <Outlet />
+      </main>
+
+      {/* footer */}
+    </div>
+  );
+};
+
+export default AppLayout;
