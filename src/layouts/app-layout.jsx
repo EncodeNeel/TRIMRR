a9b3fc98d0b7b0bf1bfeddce0047885af01b4e07
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-11 12:50:25 +0530

    🎨 Updated Tailwind CSS variables and base layer styles

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 const AppLayout = () => {
   return (
     <div>
-      <main>
+      <main className="min-h-screen container">
         {/* Header */}
         <Outlet />
       </main>
 
-      {/* footer */}
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with ❤️ by EncodeNeel
+      </div>
     </div>
   );
 };
