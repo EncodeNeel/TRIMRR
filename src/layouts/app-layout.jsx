df934b225c533459e9dbcc54c5a399299687fff8
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-11 19:05:53 +0530

    Add avatar with dropdown menu and logout functionality in Header component

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,10 +1,11 @@
+import Header from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
       <main className="min-h-screen container">
-        {/* Header */}
+        <Header />
         <Outlet />
       </main>
 
