eb42a04ba29f30172036727c1191960c3ad22a5e
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-22 00:47:20 +0530

    Add LinkCard component for displaying URL info with QR code, copy, download, and delete buttons. Update Dashboard with search filter, link creation, and loading indicators. Enhance Header with user authentication, dropdown menu, and logout functionality.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -60,8 +60,10 @@ const Header = () => {
                 <DropdownMenuSeparator />
 
                 <DropdownMenuItem>
-                  <LinkIcon className="mr-2 h-4 w-4 " />
-                  My Links
+                  <Link to="/dashboard" className="flex">
+                    <LinkIcon className="mr-2 h-4 w-4 " />
+                    My Links
+                  </Link>
                 </DropdownMenuItem>
                 <DropdownMenuItem className="text-red-400">
                   <LogOut className="mr-2 h-4 w-4" />
