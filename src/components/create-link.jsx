76edfec6586388e6a644bdb0c4611c1699016f70
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-28 02:43:10 +0530

    Fix delete URL functionality: correctly pass and use fetchUrls in LinkCard

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -3,33 +3,145 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import Error from "./error";
+import { Card } from "./ui/card";
+import { useEffect, useRef, useState } from "react";
+import * as yup from "yup";
+import QRCode from "qrcode";
+import useFetch from "@/hooks/use-fetch";
+import { createUrl } from "@/db/apiUrls";
+import { BeatLoader } from "react-spinners";
 
 const CreateLink = () => {
   const { user } = UrlState();
   const navigate = useNavigate();
+  const canvasRef = useRef();
 
   let [searchParams, setSearchParams] = useSearchParams();
   const longLink = searchParams.get("createNew");
+
+  const [errors, setErrors] = useState({});
+  const [formValues, setFormValues] = useState({
+    title: "",
+    longUrl: longLink ? longLink : "",
+    customUrl: "",
+  });
+
+  const schema = yup.object().shape({
+    title: yup.string().required("Title is required"),
+    longUrl: yup
+      .string()
+      .url("Must be valid URL")
+      .required("Long URL is required"),
+    customUrl: yup.string(),
+  });
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value });
+  };
+
+  const {
+    loading,
+    error,
+    data,
+    fn: fnCreateUrl,
+  } = useFetch(createUrl, { ...formValues, user_id: user.id });
+
+  useEffect(() => {
+    if (error === null && data) {
+      navigate(`/link/${data[0].id}`);
+    }
+  }, [error, data, navigate]);
+
+  const createNewLink = async () => {
+    setErrors({});
+    try {
+      await schema.validate(formValues, { abortEarly: false });
+
+      const canvas = canvasRef.current;
+
+      if (!canvas) {
+        console.error("Canvas ref is not available");
+        return;
+      }
+
+      await QRCode.toCanvas(canvas, formValues.longUrl);
+
+      const blob = await new Promise((resolve) => canvas.toBlob(resolve));
+
+      await fnCreateUrl(blob);
+    } catch (e) {
+      const newErrors = {};
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+      setErrors(newErrors);
+    }
+  };
+
   return (
-    <div>
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <Dialog
+      defaultOpen={!!longLink}
+      onOpenChange={(res) => {
+        if (!res) setSearchParams({});
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button variant="destructive">Create New Link</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle className="font-bold text-2xl">Create New</DialogTitle>
+        </DialogHeader>
+
+        {formValues?.longUrl && (
+          <canvas ref={canvasRef} width={250} height={250} />
+        )}
+
+        <Input
+          id="title"
+          placeholder="Short Link's Title"
+          value={formValues.title}
+          onChange={handleChange}
+        />
+        {errors.title && <Error message={errors.title} />}
+        <Input
+          id="longUrl"
+          placeholder="Enter your Loooong URL"
+          value={formValues.longUrl}
+          onChange={handleChange}
+        />
+        {errors.longUrl && <Error message={errors.longUrl} />}
+
+        <div className="flex items-center gap-2">
+          <Card className="p-2">trimrr.in</Card> /
+          <Input
+            id="customUrl"
+            placeholder="Custom Link (optional)"
+            value={formValues.customUrl}
+            onChange={handleChange}
+          />
+        </div>
+        {error && <Error message={error.message} />}
+
+        <DialogFooter className="sm:justify-start">
+          <Button
+            disabled={loading}
+            onClick={createNewLink}
+            variant="destructive"
+          >
+            {loading ? <BeatLoader size={10} color="white" /> : "Create"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
