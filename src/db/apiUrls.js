1a5b680e82c28c73bafd3f203882d63c2359b147
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-28 16:57:59 +0530

    Fix useParams and UrlState usage in Link component, correct fetch function parameters

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -68,26 +68,17 @@ export async function getLongUrl(id) {
   }
   return data;
 }
+export async function getUrl({ id, user_id }) {
+  const { data, error } = await supabase
+    .from("urls")
+    .select("*")
+    .eq("id", id)
+    .eq("user_id", user_id)
+    .single();
 
-const parser = new UAParser();
-
-export const storeClicks = async ({ id, originalUrl }) => {
-  try {
-    const res = parser.getResult();
-    const device = res.type || "desktop";
-
-    const response = await fetch("https://ipapi.co/json");
-    const { city, country_name: country } = await response.json();
-
-    await supabase.from("clicks").insert({
-      url_id: id,
-      city: city,
-      country: country,
-      device: device,
-    });
-
-    window.location.href = originalUrl;
-  } catch (error) {
-    console.error("Error recording clicks:", error);
+  if (error) {
+    console.error(error.message);
+    throw new Error("Short Url not found");
   }
-};
+  return data;
+}
--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -1,5 +1,53 @@
+import { UrlState } from "@/context";
+import { getClicksForUrl } from "@/db/apiClicks";
+import { deleteUrl, getUrl } from "@/db/apiUrls";
+import useFetch from "@/hooks/use-fetch";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
 const Link = () => {
-  return <div>Link</div>;
+  const { id } = useParams(); // Fixing useParams
+  const { user } = UrlState(); // Ensure UrlState is called as a function
+  const navigate = useNavigate();
+
+  const {
+    loading,
+    data: url,
+    fn,
+    error,
+  } = useFetch(getUrl, { id, user_id: user?.id });
+
+  const {
+    loading: loadingStats,
+    data: stats,
+    fn: fnStats,
+  } = useFetch(getClicksForUrl, id);
+
+  const { loading: loadingDelete, fn: fnDelete } = useFetch(deleteUrl, id);
+
+  useEffect(() => {
+    fn();
+    fnStats();
+  }, []);
+
+  if (error) {
+    navigate("/dashboard");
+  }
+
+  return (
+    <>
+      {(loading || loadingStats) && (
+        <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+      )}
+      <div>
+        <div>
+          <span>{url?.title}</span>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Link;
