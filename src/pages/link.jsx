c0937dfd3a191a15328ffda4dea98b6b45b234c0
Indraneel Das <dasindraneel3002@gmail.com>
2024-07-28 22:59:20 +0530

    Add LinkCard component with URL handling and update Link component with fetching and displaying URL details

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -2,13 +2,14 @@ import { UrlState } from "@/context";
 import { getClicksForUrl } from "@/db/apiClicks";
 import { deleteUrl, getUrl } from "@/db/apiUrls";
 import useFetch from "@/hooks/use-fetch";
+import { LinkIcon } from "lucide-react";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { BarLoader } from "react-spinners";
 
 const Link = () => {
-  const { id } = useParams(); // Fixing useParams
-  const { user } = UrlState(); // Ensure UrlState is called as a function
+  const { id } = useParams();
+  const { user } = UrlState();
   const navigate = useNavigate();
 
   const {
@@ -35,16 +36,41 @@ const Link = () => {
     navigate("/dashboard");
   }
 
+  let link = "";
+  if (url) {
+    link = url?.custom_url ? url?.custom_url : url.short_url;
+  }
+
   return (
     <>
       {(loading || loadingStats) && (
         <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
       )}
-      <div>
-        <div>
-          <span>{url?.title}</span>
+      <div className="flex flex-col gap-8 sm:flex-row justify-between">
+        <div className="flex flex-col items-start gap-8 rounded-lg sm:w-2/5">
+          <span className="text-6xl font-extrabold hover:underline cursor-pointer">
+            {url?.title}
+          </span>
+          <a
+            href={`https://trimrr.in/${link}`}
+            target="_blank"
+            className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
+          >
+            https://trimrr.in/{link}
+          </a>
+          <a
+            href={url?.original_url}
+            target="_blank"
+            className="flex items-center gap-1 hover:underline cursor-pointer"
+          >
+            <LinkIcon className="p-1" />
+            {url?.original_url}
+          </a>
+          <span className="flex items-end font-extralight text-sm">
+            {new Date(url?.created_at).toLocaleString()}
+          </span>
         </div>
-        <div></div>
+        <div className="sm:w-3/5"></div>
       </div>
     </>
   );
